5beeb38e788a5ea6589a221f55e48dfef184282b
Your Name <you@example.com>
2017-09-29 16:05:19 +0200

    initial commit

---
+++ src/Reader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Reader extends React.Component {
+	render() {
+		return (
+			<div>Hello World!</div>
+		)
+	}
+}
---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/index.scss';
+import Reader from "./Reader";
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<Reader/>
+		)
+	}
+}
