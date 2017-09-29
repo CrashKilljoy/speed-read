fb4c066be3a3c02186b58574d899fea04f92cc90
Your Name <you@example.com>
2017-09-29 16:55:11 +0200

    Added bootstrap css.

--- src/Reader.jsx
+++ src/Reader.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import {Button, Col, Container, Row} from "reactstrap";
+import {Button, ButtonGroup, Col, Container, Row} from "reactstrap";
 
 export default class Reader extends React.Component {
 	render() {
 		return (
-			<Container>
+			<Container className="reader">
 				<Row>
 					<Col xs={9}>
 						<div>text</div>
 					</Col>
 					<Col xs={3}>
-						<Button>Speed</Button>
-						<Button>Back</Button>
-						<Button>Pause</Button>
+						<ButtonGroup vertical>
+							<Button>Speed</Button>
+							<Button>Back</Button>
+							<Button>Pause</Button>
+						</ButtonGroup>
 					</Col>
 				</Row>
 			</Container>
