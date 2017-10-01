d7ed865e06f86741cd68e8d629792d982eab2b77
Your Name <you@example.com>
2017-10-01 19:57:52 +0200

    Fixed dimensions.

--- extension/content_script.js
+++ extension/content_script.js
@@ -15,7 +15,7 @@
 
 		const insertImage = document.createElement("iframe");
 		insertImage.setAttribute("src", browser.extension.getURL(`dist/index.html?blobURL=${request.data}`));
-		insertImage.setAttribute("style", "width: 100vw; height: 100vh;");
+		insertImage.setAttribute("style", "width: 100vw; height: 230px;");
 
 		document.body.appendChild(insertImage);
 	}
